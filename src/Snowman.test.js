2ddec8eb326d513bf73ee0e0b563a6be9a3ec9be
Brenda Liu <brenda.liu24@gmail.com>
2022-08-31 13:54:36 -0500

    finish step 2

---
+++ src/Snowman.test.js
@@ -0,0 +1,29 @@
+import { render, fireEvent } from "@testing-library/react";
+import Snowman from './Snowman';
+
+describe('test Snowman component', function () {
+
+  it('shows correct display when lose', function () {
+
+    //if nWrong = maxGuesses ... 
+    // hidden letter buttons, show you lose message, remove photo, display correct word
+    const { container } = render(<Snowman />);
+
+    fireEvent.click(container.querySelector('.z'))
+    fireEvent.click(container.querySelector('.w'))
+    fireEvent.click(container.querySelector('.y'))
+    fireEvent.click(container.querySelector('.m'))
+    fireEvent.click(container.querySelector('.h'))
+    fireEvent.click(container.querySelector('.n'))
+    
+
+    const snowmanDiv = container.querySelector('.Snowman');
+    expect(snowmanDiv).toContainHTML('You lose!');
+    expect(snowmanDiv).toContainHTML('apple');
+    // expect(snowmanDiv).not.toBeInTheDocument('img');
+    // expect("btn").not.toBeInTheDocument();
+  });
+
+
+
+});
